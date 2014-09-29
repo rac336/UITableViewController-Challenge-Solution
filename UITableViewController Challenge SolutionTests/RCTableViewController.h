4a612407af0bbb59f9aecc5ade0913c54e41230c
Reid Cohen <rac336@cornell.edu>
2014-09-29 01:16:46 -0400

    updating project

---
+++ UITableViewController Challenge SolutionTests/RCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  RCTableViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Reid Cohen on 9/1/14.
+//  Copyright (c) 2014 Reid Cohen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RCTableViewController : UITableViewController
+
+@end
